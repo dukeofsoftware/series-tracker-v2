842f3f4e79bf315de35f39b6fe247e192a719b79
Furkan Emre Kozan <kozanfurkanemre@gmail.com>
2023-08-12 23:17:12 +0300

    fix

--- src/app/[lang]/@authModal/(.)register/page.tsx
+++ src/app/[lang]/@authModal/(.)register/page.tsx
@@ -7,7 +7,7 @@ interface pageProps {}
 
 const Page: FC<pageProps> = ({}) => {
   return (
-    <div className="fixed inset-0 z-10 bg-zinc-900/20">
+    <div className="fixed inset-0 z-50 bg-zinc-900/20">
       <div className="container mx-auto flex h-full max-w-xl items-center">
         <div className="relative h-fit w-full rounded-lg bg-white px-2 py-20">
           <div className="absolute right-4 top-4">
